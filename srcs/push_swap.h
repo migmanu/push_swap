fb8b23cf4bfe266201f99f87963b189d47387dc4
migmanu42 <jmanuelmigoya@gmail.com>
2023-08-04 16:40:55 +0200

    Correct structure

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,8 +6,23 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 15:32:20 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/08/04 15:32:23 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/08/04 16:24:10 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+
+typedef struct s_stack
+{
+	long		nbr;
+	s_stack		*next;
+}	t_stack;
+
+
+#endif
