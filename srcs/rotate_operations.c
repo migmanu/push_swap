301e6e5291eaf4585072eb60a441fa1fc9b13d78
migmanu <jmanuelmigoya@gmail.com>
2023-08-07 20:14:15 +0200

    Add toggles

--- srcs/rotate_operations.c
+++ srcs/rotate_operations.c
@@ -6,57 +6,54 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 16:42:36 by migmanu           #+#    #+#             */
-/*   Updated: 2023/08/07 20:04:40 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/08/07 20:13:58 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	do_ra(t_stack **stk_a)
+void	do_ra(t_stack **stk_a, int toggle)
 {
 	t_stack	*last;
-	t_stack	*new_last;
+	t_stack	*buf;
 
 	if (!*stk_a || !(*stk_a)->next)
-	{
-		printf("nope\n");
 		return ;
-	}
 	last = ft_lstlast(*stk_a);
-	new_last = *stk_a;
-	while (new_last->next != last)
-		new_last = new_last->next;
-	new_last->next = NULL;
+	buf = *stk_a;
+	while (buf->next != last)
+		buf = buf->next;
+	buf->next = NULL;
 	last->next = *stk_a;
 	*stk_a = last;
-	write(1, "ra\n", 3);
+	if (toggle == 1)
+		write(1, "ra\n", 3);
 }
 
-void	do_rb(t_stack **stk_a)
+void	do_rb(t_stack **stk_a, int toggle)
 {
 	t_stack	*last;
-	t_stack	*new_last;
+	t_stack	*buf;
 
 	if (!*stk_a || !(*stk_a)->next)
-	{
-		printf("nope\n");
 		return ;
-	}
 	last = ft_lstlast(*stk_a);
-	new_last = *stk_a;
-	while (new_last->next != last)
-		new_last = new_last->next;
-	new_last->next = NULL;
+	buf = *stk_a;
+	while (buf->next != last)
+		buf = buf->next;
+	buf->next = NULL;
 	last->next = *stk_a;
 	*stk_a = last;
-	write(1, "rb\n", 3);
+	if (toggle == 1)
+		write(1, "rb\n", 3);
 }
 
-void	do_rr(t_stack **stk_a, t_stack **stk_b)
+void	do_rr(t_stack **stk_a, t_stack **stk_b, int toggle)
 {
-	do_ra(stk_a);
-	do_rb(stk_b);
-	write(1, "rr\n", 3);
+	do_ra(stk_a, 0);
+	do_rb(stk_b, 0);
+	if (toggle == 1)
+		write(1, "rr\n", 3);
 }
 
 int	main(void)
@@ -65,9 +62,9 @@ int	main(void)
 	t_stack	*stk_b;
 	stk_a = tst_make_stack(4);
 	stk_b = tst_make_stack(4);
-	//do_ra(&stk_a);
-	//do_rb(&stk_b);
-	//do_rr(&stk_a, &stk_b);
+	//do_ra(&stk_a, 1);
+	//do_rb(&stk_b, 1);
+	//do_rr(&stk_a, &stk_b, 0);
 	printf("\nstack A\n");
 	tst_print_stack(&stk_a);
 	printf("\nstack B\n");
