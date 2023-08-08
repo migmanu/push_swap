888f8b673978484ec74410c9d6b8279417cfe6a2
migmanu42 <jmanuelmigoya@gmail.com>
2023-08-08 16:56:13 +0200

    Take user input

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,8 +6,24 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 15:32:26 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/08/07 16:16:08 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/08/08 16:51:19 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+		return (0);
+	if (argc == 2)
+	{
+		printf("Two args\n");
+		check_str(argv[1]);
+	}
+	else
+	{
+
+	}
+	return (0);
+}
