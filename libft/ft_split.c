9a81c1b4af8b46cdd0af5965e6b005ca973672bf
migmanu42 <jmanuelmigoya@gmail.com>
2023-08-14 17:26:01 +0200

    Redoo!

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,48 +6,25 @@
 /*   By: jmigoya- jmigoya-@student.42berlin.de      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 19:22:49 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/08/11 19:59:45 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/08/14 17:24:24 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_allocate(char **vec, const char *s, char c)
+int	get_vec_size(const char *s, char c)
 {
-	int	i;
-	int	k;
-	int	str_nbr;
-
-	i = 0;
-	str_nbr = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] != c)
-		{
-			k = i;
-			while (s[k] != c && s[k] != '\0')
-				k++;
-			vec[str_nbr] = ft_substr(s, i, (k - i));
-			str_nbr++;
-			i = k;
-		}
-		i++;
-	}
-	vec[str_nbr] = NULL;
-}
-
-int	ft_count_strs(const char *s, char c)
-{
-	int	i;
 	int	result;
+	int	i;
 
+	result = 0;
 	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] != c)
 		{
-			result += 1;
-			while (s[i + 1] != c && s[i + 1] != '\0')
+			result++;
+			while (s[i] != c && s[i + 1] != '\0')
 				i++;
 		}
 		i++;
@@ -55,32 +32,41 @@ int	ft_count_strs(const char *s, char c)
 	return (result);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(const char *s, char c)
 {
-	char	**vec;
-	int		size;
+	char **vec;
+	int	end;
+	int	start;
 
-	if (!s)
-		return (NULL);
-	size = ft_count_strs(s, c);
-	vec = malloc(sizeof(char *) * size + 1);
-	if (!vec)
-		return (NULL);
-	ft_allocate(vec, s, c);
+	vec = malloc((get_vec_size(s, c) + 1) * sizeof(char *));
+	if (!s || !vec)
+		return (0);
+	vec[get_vec_size(s, c)] = NULL;
+	i = 0;
+	while (s[end] != '\0')
+	{
+		while (s[end] == c)
+			end++;
+		start = end;
+		while (s[end] != c && s[end] != '\0')
+			end++;
+	}
 	return (vec);
 }
 
-int main(int argc, char *argv[])
-{
-    char *s = argv[1];
-    char c = ' ';
-	//printf("strs: %d\n", ft_count_strs(s, c));
-	printf("str: %s\n", s);
 
+int main(void)
+{
+    char *s = "";
+    char c = ' ';
+	printf("%d", get_vec_size(s, c));
+/*
     char **res = ft_split(s, c);
-    if (res)
+	if (!res)
+		return (0);
+    if (*res)
     {
-        int i = 0;
+		int i = 0;
         while (res[i])
         {
 			printf("%s\n", res[i]);
@@ -89,6 +75,6 @@ int main(int argc, char *argv[])
             i++;
         }
     }
-	free(res);
+	free(res);*/
     return 0;
 }
