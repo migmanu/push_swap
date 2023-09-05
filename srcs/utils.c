97b9e0a02e28d03661103f93384b8eb19c5027a2
migmanu <jmigoya@gmail.com>
2023-09-05 19:32:26 +0200

    Add comment

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,12 +6,15 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:32:03 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/08/18 16:32:30 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/09/05 19:32:12 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+// In case user inputs a string of numbers. vec_size counts the amount
+// of numbers present. It does so by using char c as a separator (in 
+// this case it should only be a space char).
 int	vec_size(const char *s, char c)
 {
 	int	result;
