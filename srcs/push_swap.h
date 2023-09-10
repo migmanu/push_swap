5cf81ac44dfe116f99836419280647b7f27aa110
migmanu <jmigoya@gmail.com>
2023-09-10 19:26:53 +0200

    Add comments to detail files

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 15:32:20 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/09/10 18:45:01 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/09/10 19:25:20 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,41 +35,52 @@ typedef enum e_bool
 # define FALSE false
 # define TRUE true
 
+// control_args.c
 t_bool	check_str(char *str, int toggle);
 t_bool	check_args(int argc, char *argv[]);
 void	check_numbers(t_stk *stk);
 
+// test_functions.c
 void	tst_add_node_end(t_stk **stack, t_stk *new);
 void	tst_print_stk(t_stk *root);
 t_stk	*tst_make_stack(int n_nodes, int max, int rand);
 t_stk	*tst_make_arr_stk(int *arr, int n);
 
+//utils.c
 int		vec_size(const char *s, char c);
 
+// stk_utils.c
 t_bool	stk_add_end(t_stk **stk, t_stk *new_node);
 t_stk	*stk_get_last(t_stk *stk);
 t_stk	*stk_get_min(t_stk *stk);
 t_stk	*stk_get_max(t_stk *stk);
 int		stk_cnt_nds(t_stk *stk);
 
+// create_stack.c
 t_stk	*create_stack(int argc, char **vec);
 
+// rotation_operations.c
 t_stk	*rotate(t_stk *stk);
 t_stk	*rev_rotate(t_stk *stk);
-t_stk	*get_previous(t_stk *stk, t_stk *node);
-int		rot_to_top(t_stk *(*f) (t_stk *), t_stk *stk, t_stk *node);
+t_bool	sync_rotate(t_stk **stk_a, t_stk **stk_b);
+t_bool	sync_rev_rotate(t_stk **stk_a, t_stk **stk_b);
 
-t_stk	*find_cheapest(t_stk *stk_a, t_stk *stk_b);
-int		get_sync_cst(t_stk *stk_a, t_stk *stk_b, t_stk *node);
-int		get_rev_rot_cost(t_stk *stk, t_stk *node);
-int		get_rot_cost(t_stk *stk, t_stk *node);
-
+// push_operations.c
 void	push(t_stk **src, t_stk **dest);
 
+// simulate.c
 t_stk	*find_cheapest(t_stk *stk_a, t_stk *stk_b);
 int		get_unsync_cst(t_stk *stk_a, t_stk *stk_b, t_stk *node);
 int		get_sync_cst(t_stk *stk_a, t_stk *stk_b, t_stk *node);
 int		get_rev_rot_cost(t_stk *stk, t_stk *node);
 int		get_rot_cost(t_stk *stk, t_stk *node);
+
+// simulate_utils.c
+t_stk	*get_previous(t_stk *stk, t_stk *node);
+
+// sync_rotations.c
+t_bool	sync_r_or_rr(t_stk *stk_a, t_stk *stk_b, t_stk *node);
+void	sync_rotate(t_stk **stk_a, t_stk **stk_b, t_stk *node);
+void	sync_to_top(t_stk **stk_a, t_stk **stk_b, t_stk *node);
 
 #endif
