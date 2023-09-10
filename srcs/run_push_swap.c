67a093d411dcda0212696480e118eaf140988886
migmanu <jmigoya@gmail.com>
2023-09-10 20:44:47 +0200

    Add functions and comments

--- srcs/run_push_swap.c
+++ srcs/run_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 17:26:30 by migmanu           #+#    #+#             */
-/*   Updated: 2023/09/10 18:52:24 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/09/10 20:43:52 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,64 +20,6 @@ void	push_two(t_stk **stk_a, t_stk **stk_b)
 		push(stk_a, stk_b);
 	if (stk_cnt_nds(*stk_a) > 3)
 		push(stk_a, stk_b);
-}
-
-// Decides wether rotation or rev rotation is cheaper for a sync move.
-// Returns true if rotations is cheaper and false if rev_rotation is cheaper
-t_bool	sync_r_or_rr(t_stk *stk_a, t_stk *stk_b, t_stk *node)
-{
-	t_stk	*prev;
-	int		rotation;
-	int		rev_rotation;
-
-	prev = get_previous(stk_b, node);
-	rotation = get_rot_cost(stk_a, node);
-	rev_rotation = get_rev_rot_cost(stk_a, node);
-	if (get_rot_cost(stk_b, prev) < rotation)
-		rotation = get_rot_cost(stk_b, prev);
-	if (get_rev_rot_cost(stk_b, prev) < rev_rotation)
-		rev_rotation = get_rev_rot_cost(stk_b, prev);
-	if (rotation < rev_rotation)
-		return (true);
-	return (false);
-}
-
-void	sync_rotate(t_stk **stk_a, t_stk **stk_b, t_stk *node)
-{
-
-	while (*stk_a != node && *stk_b != prev)
-	{
-		//rotate A
-		//rotate B
-		//write
-	}
-	while (stk_a != node)
-	{
-		// rotate A
-		// write
-	}
-	while (stk_b != prev)
-	{
-		// rotate B
-		// write
-	}
-}
-
-// Depending on result of sync_r_or_rr calls for sync_rotate
-// or sync_rev_rotate
-void	sync_to_top(t_stk **stk_a, t_stk **stk_b, t_stk *node)
-{
-	t_stk	*prev;
-
-	prev = get_previous(stk_b, node);
-	if (sync_r_or_rr(*stk_a, *stk_b, node) == true)
-	{
-		// perform sync rotation
-	}
-	else
-	{
-		// perform sync rev_rotation
-	}
 }
 
 // Takes care of pushing the cheapest node from stk_a
