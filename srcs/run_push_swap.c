79be40954890389ca24556638abc736dbf036d98
migmanu <jmigoya@gmail.com>
2023-09-13 22:45:08 +0200

    Free stk_a and stk_b

--- srcs/run_push_swap.c
+++ srcs/run_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 17:26:30 by migmanu           #+#    #+#             */
-/*   Updated: 2023/09/13 18:50:51 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/09/13 22:43:21 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,21 @@ t_stk	*push_two(t_stk **stk_a)
 		push(stk_a, &stk_b);
 	}
 	return (stk_b);
+}
+
+// Function used to put stk_a in ascending order, afer runing
+// push_to_stk_a.
+void	order_stk_a(t_stk **stk_a, t_stk *min)
+{
+
+	if (get_rot_cost(*stk_a, min) < get_rev_rot_cost(*stk_a, min))
+	{
+		write_n_rotate(stk_a, min, 1);
+	}
+	else
+	{
+		write_n_rev_rotate(stk_a, min, 1);
+	}
 }
 
 // Takes care of pushing the cheapest node from stk_a
@@ -61,6 +76,7 @@ void push_to_stk_b(t_stk **stk_a, t_stk **stk_b)
 		tst_print_stk(*stk_a);
 		printf("stk_b\n");
 		tst_print_stk(*stk_b);
+		write(1, "pb\n", 3);
 		push(stk_a, stk_b);
 	}
 }
@@ -74,14 +90,20 @@ t_bool	run_push_swap(t_stk **stk_a)
 	stk_b = push_two(stk_a);
 	push_to_stk_b(stk_a, &stk_b);
 	solve_three(stk_a);
+	push_to_stk_a(stk_a, &stk_b);
+	order_stk_a(stk_a, stk_get_min(*stk_a));
+	free_stk(stk_b);
+	free_stk(*stk_a);
 	return (true);
 }
 
 int	main(void)
 {
 	t_stk	*stk_a;
+	int	arr1[] = {6, 543, 1};
 
-	stk_a = tst_make_stack(6, 543, 1);
+	stk_a = tst_make_stack(5, 543, 1);
+	//stk_a = tst_make_arr_stk(arr1, 3);
 	//stk_b = tst_make_stack(2, 53, 1);
 	run_push_swap(&stk_a);
 	return (0);
